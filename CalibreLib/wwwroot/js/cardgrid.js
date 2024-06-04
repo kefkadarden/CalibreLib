bfab8284da7e5be99b959266a5625424d3fdcd62
Kefka D'Arden <gnomeforest@hotmail.com>
2024-06-03 23:48:14 -0400

    Added rest of the pagination code. Related Work Items: #5

--- CalibreLib/wwwroot/js/cardgrid.js
+++ CalibreLib/wwwroot/js/cardgrid.js
@@ -1,6 +1,7 @@
 ﻿
 var ajaxCallUrl = 'CardGrid/BookList',  
     page = 0,  
+    pagingEnabled = false,
     sortBy = 'datedesc',
     inCallback = false,  
     isReachedScrollEnd = false;  
@@ -18,12 +19,43 @@ function updateSortBy(_sortBy) {
     loadBooks(ajaxCallUrl);
 }  
 var scrollHandler = function () {  
-    if (isReachedScrollEnd == false &&  
+    if (isReachedScrollEnd == false && pagingEnabled == false &&  
         ($(document).scrollTop() <= $(document).height() - $(window).height())) {  
         loadBooks(ajaxCallUrl);  
     }  
 }  
-function loadBooks(ajaxCallUrl) {   
+
+function togglePagingEnabled(enable) {
+    page = 0;
+    isReachedScrollEnd = false;
+    pagingEnabled = enable;
+    $("#cardGridRow").empty();
+    loadBooks(ajaxCallUrl);
+    if (enable)
+        $('#paginationToolbar').removeClass('visually-hidden');
+    else
+        $('#paginationToolbar').addClass('visually-hidden');
+}
+
+function pagingNavigation(direction) {
+    
+    if (direction === 'Prev') {
+        if (page <= 1) {
+            page = 1;
+            return;
+        }
+        page-=2;
+    }
+    let lbl = document.getElementById('lblPageNum');     
+    loadBooks(ajaxCallUrl, true);
+    lbl.value = page;
+}
+
+function updatePageLabel() {
+    $('#lblPageNum')[0].innerText = page;
+}
+
+function loadBooks(ajaxCallUrl, isPaging) {  
     if (page > -1 && !inCallback) {  
         inCallback = true;  
         $("div#loading").show();  
@@ -33,11 +65,21 @@ function loadBooks(ajaxCallUrl) {
             data: "pageNumber=" + page++ + "&sortBy=" + sortBy,  
             success: function (data, textstatus) { 
                 if (data.replace(/(\r\n|\n|\r)/gm, "") != '') {  
+                    if (isPaging)
+                        $("#cardGridRow").empty();
+
                     $("#cardGridRow").append(data);  
+                    updatePageLabel();
                 }  
                 else {  
-                    page = -1;  
-                    isReachedScrollEnd = true;
+                    if (!isPaging) {
+                        page = -1;
+                        isReachedScrollEnd = true;
+                    } else {
+                        page--;
+                        isReachedScrollEnd = true;
+                        updatePageLabel();
+                    }                
                 }  
   
                 inCallback = false;  
@@ -47,5 +89,5 @@ function loadBooks(ajaxCallUrl) {
                 alert(errorThrown);  
             }  
         });  
-    }  
+    }      
 }  
