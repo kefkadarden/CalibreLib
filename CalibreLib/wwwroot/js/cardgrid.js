8d446a7f3d28aa6286c98e706aecb62a654d982a
Kefka D'Arden <kefka.darden@gmail.com>
2025-12-22 02:18:34 -0500

    Removed pagination and other old code

--- CalibreLib/wwwroot/js/cardgrid.js
+++ CalibreLib/wwwroot/js/cardgrid.js
@@ -1,10 +1,8 @@
 ﻿var ajaxCallUrl = "/CardGrid/BookList",
   page = 0,
-  //pagingEnabled = false,
   sortBy = localStorage.getItem("gridSortBy") ?? "datedesc",
   pageSize = localStorage.getItem("gridPageSize") ?? 30,
   inCallback = false,
-  //isReachedScrollEnd = false,
   shelf = null,
   category = null,
   author = null,
@@ -13,26 +11,21 @@
   rating = null,
   series = null,
   archived = null,
-  //pageCount = 0,
-  disableJS = false,
   currentPageType = "",
   filterBy,
   $scroll;
 
 function toggleScroll() {
-  if (disableJS) {
-    $("#cardGridRow").infiniteScroll("destroy");
-  } else {
-    $scroll = $("#cardGridRow").infiniteScroll({
-      path: function () {
-        page = this.loadCount;
+  // $("#cardGridRow").infiniteScroll("destroy");
+  $scroll = $("#cardGridRow").infiniteScroll({
+    path: function () {
+      page = this.loadCount;
 
-        return ajaxCallUrl + "?" + loadBooksQuery(false);
-      },
-      append: ".grid-item",
-      history: false,
-    });
-  }
+      return ajaxCallUrl + "?" + loadBooksQuery(false);
+    },
+    append: ".grid-item",
+    history: false,
+  });
 }
 function updateSortBy(_sortBy) {
   localStorage.setItem("gridSortBy", _sortBy);
@@ -93,74 +86,6 @@ function loadListViewComponent(type, filter, sortBy) {
     },
   });
 }
-//var scrollHandler = function () {
-//    if (isReachedScrollEnd == false && pagingEnabled == false &&
-//        ($(document).scrollTop() <= $(document).height() - $(window).height())) {
-//        loadBooks(ajaxCallUrl);
-//    }
-//}
-
-//function togglePagingEnabled(enable) {
-//    page = 0;
-//    isReachedScrollEnd = false;
-//    pagingEnabled = enable;
-
-//    toggleScroll();
-//    localStorage.setItem('gridPagingEnabled', enable);
-//    const btnPage = $('#btnPage')[0];
-//    btnPage.checked = enable;
-//    $("#cardGridRow").empty();
-//    if (enable) {
-//        $('#paginationToolbar').removeClass('visually-hidden');
-//        $('#divPageSize').removeClass('visually-hidden');
-//        pageSize = localStorage.getItem('gridPageSize') ?? 10;
-//    }
-//    else {
-//        $('#paginationToolbar').addClass('visually-hidden');
-//        $('#divPageSize').addClass('visually-hidden');
-//        pageSize = 30;
-//    }
-//    //loadBooks(ajaxCallUrl);
-//}
-
-//function pagingNavigation(direction) {
-
-//    if (direction === 'Prev') {
-//        if (page <= 1) {
-//            page = 1;
-//            return;
-//        }
-//        page-=2;
-//    }
-//    let lbl = document.getElementById('lblPageNum');
-//    loadBooks(ajaxCallUrl, true);
-//    lbl.value = page;
-//}
-
-//function updatePageLabel() {
-//    if ($('#lblPageCount').length > 0)
-//        $('#lblPageNum')[0].innerText = page;
-//}
-
-//function setPageCount() {
-//    if ($('#lblPageCount').length > 0) {
-//        if (pagingEnabled) {
-//            $.ajax({
-//                type: 'GET',
-//                url: '/CardGrid/GetPageCount',
-//                data: loadBooksQuery(false),
-//                success: function (data) {
-//                    $('#lblPageCount')[0].innerText = "Pages: " + data.pageCount;
-//                },
-//                error: function (XMLHttpRequest, textStatus, errorThrown) {
-//                    alert(errorThrown);
-//                }
-//            });
-//        } else {
-//            $('#lblPageCount')[0].innerText = 'Pages: N/A';
-//        }
-//    }
-//}
 
 async function getPageCount() {
   $.ajax({
@@ -213,81 +138,43 @@ function loadBooksQuery(pageIncrement = false) {
   return url;
 }
 
-function loadBooks(ajaxCallUrl, isPaging) {
-  if (page > -1 && !inCallback) {
-    inCallback = true;
-    showLoading();
-    $.ajax({
-      type: "GET",
-      url: ajaxCallUrl,
-      data: loadBooksQuery(false),
-      success: function (data, textstatus) {
-        if (data.replace(/(\r\n|\n|\r)/gm, "") != "") {
-          if (isPaging) $("#cardGridRow").empty();
-          $("#cardGridRow").append(data);
-
-          //var $items = $(data);
-          //$grid.isotope('appended', $items);
-          //updatePageLabel();
-        } else {
-          if (!isPaging) {
-            page = -1;
-            //isReachedScrollEnd = true;
-          } else {
-            page--;
-            //isReachedScrollEnd = true;
-            //updatePageLabel();
-          }
-        }
-
-        inCallback = false;
-        hideLoading();
-      },
-      error: function (XMLHttpRequest, textStatus, errorThrown) {
-        hideLoading();
-        alert(errorThrown);
-      },
-    });
-  }
-  //setPageCount();
-}
-
-//window.onload = () => {
-
-//    let lblPageNum = document.getElementById('lblPageNum');
-
-//    if (!lblPageNum)
-//        return;
-//    lblPageNum.addEventListener('keyup', (event) => {
-//        if (event.key === 'Enter') {
-//            page = lblPageNum.value - 1; //loadBooks increments so need to set page to prior number so it increments to the number entered.
-//            loadBooks(ajaxCallUrl, true);
-//            lblPageNum.value = page;
-//        }
-//    });
-//    lblPageNum.addEventListener('keypress', (event) => {
-//        if (!Number.parseInt(event.key) && event.key != '0') {
-//            event.preventDefault();
-//        }
-//    });
-
-//    let lblPageSize = document.getElementById('lblPageSize');
-
-//    lblPageSize.addEventListener('keyup', (event) => {
-//        if (event.key === 'Enter') {
-//            localStorage.setItem('gridPageSize', lblPageSize.value);
-//            pageSize = lblPageSize.value;
-//            page = lblPageNum.value - 1; //loadBooks increments so need to set page to prior number so it increments to the number entered.
-//            loadBooks(ajaxCallUrl, true);
-//            lblPageNum.value = page;
-//        }
-//    });
-//    lblPageSize.addEventListener('keypress', (event) => {
-//        if (!Number.parseInt(event.key) && event.key != '0') {
-//            event.preventDefault();
-//        }
-//    });
-//}
+// function loadBooks(ajaxCallUrl, isPaging) {
+//   if (page > -1 && !inCallback) {
+//     inCallback = true;
+//     showLoading();
+//     $.ajax({
+//       type: "GET",
+//       url: ajaxCallUrl,
+//       data: loadBooksQuery(false),
+//       success: function (data, textstatus) {
+//         if (data.replace(/(\r\n|\n|\r)/gm, "") != "") {
+//           if (isPaging) $("#cardGridRow").empty();
+//           $("#cardGridRow").append(data);
+//
+//           //var $items = $(data);
+//           //$grid.isotope('appended', $items);
+//           //updatePageLabel();
+//         } else {
+//           if (!isPaging) {
+//             page = -1;
+//             //isReachedScrollEnd = true;
+//           } else {
+//             page--;
+//             //isReachedScrollEnd = true;
+//             //updatePageLabel();
+//           }
+//         }
+//
+//         inCallback = false;
+//         hideLoading();
+//       },
+//       error: function (XMLHttpRequest, textStatus, errorThrown) {
+//         hideLoading();
+//         alert(errorThrown);
+//       },
+//     });
+//   }
+// }
 
 async function downloadBook(button) {
   console.log(button.getAttribute("data-url"));
